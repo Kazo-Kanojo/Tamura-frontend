6255d49e6eb4574cfb85efe036f732d78bbeb95c
Enzo <enzopyton24@gmail.com>
2025-11-27 17:02:01 -0300

    corrigindo api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,4 @@
 // src/api.js
-// Se estiver na internet (Vercel), usa o link do Render.
-// Se estiver no seu PC, usa localhost:3000.
-const API_URL = import.meta.env.VITE_API_URL || 'https://tamura-api.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 export default API_URL;
