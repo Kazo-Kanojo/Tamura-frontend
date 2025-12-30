be7bcd3561d8709df9cf13bfe2a2cb5e23f9bfba
Enzo <enzopyton24@gmail.com>
2025-12-30 01:09:46 -0300

    Criadno o PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      // As configurações do manifesto são puxadas automaticamente do arquivo public/manifest.json
+      // Mas precisamos garantir que o service worker seja gerado corretamente
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true // Permite testar o PWA em ambiente de desenvolvimento
+      }
+    })
   ],
+  server: {
+    host: '0.0.0.0',
+  }
 })
